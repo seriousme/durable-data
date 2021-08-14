dac4336591811eb9bc3ac55ecc6be5976d4bf2c3
Hans Klunder <hans.klunder@gmail.com>
2021-08-14 10:49:17 +0200

    Add creation of testdb dir

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-deno test --doc --import-map doc.importmap.json --allow-read=dbdir --allow-write=dbdir
+#!/bin/sh
+DB_DIR="dbdir"
+mkdir -p  $DB_DIR
+deno test --doc --import-map doc.importmap.json --allow-read=$DB_DIR --allow-write=$DB_DIR
